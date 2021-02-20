447160f252554aadbc9cbc65aa663a0aaf068a44
Zachary Vincze <zacharyvincze@gmail.com>
2021-02-20 07:06:03 -0500

    Creating user subdirectories for program uploads

--- pi_server/ComputeHotspot/client/handlers.py
+++ pi_server/ComputeHotspot/client/handlers.py
@@ -5,28 +5,35 @@ import uuid
 import paramiko
 
 
-def handle_uploaded_file(f):
-    file_path = "files/" + str(uuid.uuid4()) + ".py"
+def handle_uploaded_file(f, user_id):
+    file_path = "files/" + user_id + ".py"
     with open(file_path, "wb+") as destination:
         for chunk in f.chunks():
             destination.write(chunk)
     return file_path
 
 
-def copy_file_to_remote_machine(filepath, remote_filepath, hostname):
+def copy_file_to_remote_machine(filepath, user_id, username, hostname):
+    ssh = paramiko.SSHClient()
+    key = paramiko.RSAKey.from_private_key_file(
+        "/home/zacharyvincze/.ssh/id_rsa")
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname, username=username, pkey=key)
+    ssh.exec_command(
+        "mkdir files/" + user_id)
     subprocess.run(
-        ["scp", filepath, hostname + ":" + remote_filepath])
+        ["scp", filepath, username + "@" + hostname + ":" + "~/files/" + user_id + "/" + user_id + ".py"])
     os.remove(filepath)
 
 
-def execute_file_on_remote_machine(filepath, runtime, username, hostname):
+def execute_file_on_remote_machine(user_id, runtime, username, hostname):
     ssh = paramiko.SSHClient()
     key = paramiko.RSAKey.from_private_key_file(
         "/home/zacharyvincze/.ssh/id_rsa")
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(hostname, username=username, pkey=key)
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(
-        "python3.8 programhandler.py " + runtime + " " + filepath)
+        "python3.8 programhandler.py " + runtime + " " + "files/" + user_id + "/" + user_id + ".py")
     result = []
     for line in iter(ssh_stdout.readline, ""):
         result.append(line)
--- pi_server/ComputeHotspot/client/views.py
+++ pi_server/ComputeHotspot/client/views.py
@@ -14,10 +14,11 @@ def index(request):
     elif request.method == 'POST':
         form = ComputeForm(request.POST, request.FILES)
         if form.is_valid():
+            user_id = str(uuid.uuid4())
             filepath = handlers.handle_uploaded_file(
-                request.FILES['file'])
+                request.FILES['file'], user_id)
             handlers.copy_file_to_remote_machine(
-                filepath, "~/files/.", "pi_01@52.142.54.92")
+                filepath, user_id, "pi_01", "52.142.54.92")
             result = handlers.execute_file_on_remote_machine(
                 filepath, request.POST['runtime'], "pi_01", "52.142.54.92")
             context = {
