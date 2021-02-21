81eb2e105d36e4d3fbb8607d0e56fac70e546bfc
Zachary Vincze <zacharyvincze@gmail.com>
2021-02-21 00:51:55 -0500

    Hopefully fixed

--- pi_server/ComputeHotspot/ComputeHotspot/urls.py
+++ pi_server/ComputeHotspot/ComputeHotspot/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('client.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
